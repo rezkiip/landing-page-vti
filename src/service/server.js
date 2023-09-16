8a75af1d244c1d4830e78d13de791402a1f012b5
rezki <rezki.pratama@pactindo.com>
2023-09-16 22:42:06 +0700

    update rest api base url

--- src/service/server.js
+++ src/service/server.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // import FunctionService from '../tools/FunctionService';
 
-const baseURL = 'http://156.67.219.199:8087/api/v1';
+const baseURL = 'https://ideo.id/api/v1';
 
 const AXIOS = axios.create({
   baseURL
